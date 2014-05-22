27b5ef50d5683c1ac1ac9cb58957997a0c2bc054
Jozsef Nyitrai <nyitrai.jozsef@gmail.com>
2014-05-22 12:05:31 +0200

    Update code style according to ruby style guide

--- lib/generators/devise/views/i18n_templates/i18n_templates_generator.rb
+++ lib/generators/devise/views/i18n_templates/i18n_templates_generator.rb
@@ -3,7 +3,7 @@ module Devise
     class I18nTemplatesGenerator < Rails::Generators::Base
       source_root File.expand_path('../../../../../../app/views', __FILE__)
       def copy_views
-        directory("devise", File.join("app", "views", "devise"))
+        directory('devise', File.join('app', 'views', 'devise'))
       end
     end
   end
