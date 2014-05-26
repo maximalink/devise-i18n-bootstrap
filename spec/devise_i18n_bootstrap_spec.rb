badea2a26189a4cc960a63e9f8ae0d682b8a064a
Jozsef Nyitrai <nyitrai.jozsef@gmail.com>
2014-05-26 22:52:54 +0200

    Full translation for Hungarian language

--- spec/devise_i18n_bootstrap_spec.rb
+++ spec/devise_i18n_bootstrap_spec.rb
@@ -6,3 +6,7 @@ Dir.glob('config/locales/*.yml').each do |locale_file|
     it { locale_file.should be_a_subset_of 'config/locales/en.yml' }
   end
 end
+
+describe 'Check missing translations from hu.yml that' do
+  it { 'config/locales/en.yml'.should be_a_subset_of 'config/locales/hu.yml' }
+end
